bdf8da907f1b0e5f9144068c8dffd7d1f0f36f1c
Evan Palsson <evanpalsson@gmail.com>
2015-01-12 08:44:40 -0700

    adding Fridays work

--- practice.js
+++ practice.js
@@ -1,5 +1,6 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
+Problem #1
 
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only argument.
@@ -7,40 +8,89 @@ var name = 'Tyler';
 
   //Code Here
 
+var isTyler = function(name) {
+  if(name === 'Tyler') {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-//Next problem
+isTyler('Tyler')  <--- This will return true
+isTyler('Evan') <--- This will return false
 
 
+
+
+
+//Next problem --------------------------------------------------------------------
+
+Problem #2
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
-
-  //Code Here
-
-
-//Next Problem
+ //Code Here
 
 
+var getName = function() {
+  var userName = prompt('Your name');  // <--- Not understanding why this need parens (display strong vs variable?)
+    return userName;
+}
+
+
+getName()
+
+
+
+
+
+//Next problem --------------------------------------------------------------------
+
+Problem #3
 
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
 
+var welcome = function(getName) {
+  alert("Welcome, " + getName);  // <--- The + getName is getName and not userName because
+}
 
-//Next problem
+welcome(getName)  // <--- I am being told by the console that 'userName is not defined'. well, i want it to prompt me to get it defined, but it is not executing.
+
+welcome(getName()) // <--- This is correct because the function is calling a function
 
 
 
-//Write a function called adder that uses the 'arguments' keyword that comes with every function to
-//loop over all the arguments the function received, add them all together, then return the result.
+
+
+//Next problem --------------------------------------------------------------------
+
+Problem #4
+
+//Write a function called adder that uses the 'arguments' keyword that comes with every function to loop over all the arguments the function received, add 
+// them all together, then return the result.
 //Save the result of the return value from your adder function, then alert "The Total Number was " + the number that was
 //returned from adder.
 
   //Code Here
 
+var adder = function() {
+  var total = 0;
+    for (var i = 0; i < arguments.length; i++) {
+      total = total + arguments[i];
+    }
+  return total;
+}
 
-//Next Problem
+adder(500, 300, 200);
+
+
+
+
+//Next problem --------------------------------------------------------------------
 
 
 
@@ -49,7 +99,7 @@ var name = 'Tyler';
   //Answer Here
 
 
-//Next problem
+//Next problem --------------------------------------------------------------------
 
 
 
@@ -60,7 +110,7 @@ var name = 'Tyler';
 
 
 
-//Next Problem
+//Next problem --------------------------------------------------------------------
 
 
 
@@ -76,7 +126,7 @@ var name = 'Tyler';
 
 
 
-//Next problem
+//Next problem --------------------------------------------------------------------
 
 
 
